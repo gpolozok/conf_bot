3029659becf885118cff6bd721ce487848d96069
gpolozok <63362714+gpolozok@users.noreply.github.com>
2020-09-17 00:57:07 +0300

    Add files via upload

---
+++ ConfBot.py
@@ -0,0 +1,196 @@
+import requests
+import re
+import sqlite3
+from bs4 import BeautifulSoup
+import warnings
+import functools
+import datetime
+
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+    return new_func
+
+
+class Bot:
+
+    def __init__(self, token):
+        self.__token = token
+        self.__api_url = 'https://api.telegram.org/bot{}/'.format(self.__token)
+        self.__now = datetime.datetime.now()
+        self.__weather_url = 'https://www.meteoservice.ru/weather/now/moskva'
+        self.__covid_url = 'https://api.thevirustracker.com/free-api?countryTotal=RU'
+
+    @deprecated
+    def create_db(self):
+        conn = sqlite3.connect('bot_bd.db')
+        c = conn.cursor()
+        c.execute('CREATE TABLE "episodes" '
+                  '("NUMBER"    INTEGER NOT NULL UNIQUE, '
+                  '"NAME"       TEXT NOT NULL UNIQUE);')
+        conn.commit()
+        c.close()
+
+    def get_updates(self, offset=None, timeout=0):
+        method = 'getUpdates'
+        params = {'offset': offset, 'timeout': timeout}
+        response = requests.get(self.__api_url + method, params)
+        return response.json()['result']
+
+    def last_update(self, offset=None, timeout=0):
+        result = self.get_updates(offset, timeout)
+        if(len(result) > 0):
+            return result[-1]
+        else:
+            return 'error'
+
+    def get_chat_id(self, last_update):
+        return last_update['message']['chat']['id']
+
+    def get_message_id(self, last_update):
+        return last_update['message']['message_id']
+
+    def get_chat_text(self, last_update):
+        return last_update['message']['text']
+
+    def get_type(self, last_update):
+        return last_update['message']['chat']['type']
+
+    def get_chat_name(self, last_update, last_type):
+        if last_type == 'private':
+            return last_update['message']['chat']['first_name']
+        elif last_type == 'group':
+            return last_update['message']['from']['first_name']
+
+    def send_mailing(self, last_update, chat_id, text):
+        if last_update['message']['chat']['username'] == 'grisha1505':
+            params = {
+                'chat_id': chat_id,
+                'text': text
+            }
+            method = 'sendMessage'
+            response = requests.post(self.__api_url + method, params)
+            return response
+
+    def send_message(self, chat_id, text, reply_id=None):
+        params = {
+            'chat_id': chat_id,
+            'text': text,
+            'reply_to_message_id': reply_id
+        }
+        method = 'sendMessage'
+        response = requests.post(self.__api_url + method, params)
+        return response
+
+    def send_photo(self, chat_id, photo_url):
+        params = {
+            'chat_id': chat_id,
+            'photo': photo_url
+        }
+        method = 'sendPhoto'
+        response = requests.post(self.__api_url + method, params)
+        return response
+
+    def set_chat_title(self, chat_id, title):
+        conn = sqlite3.connect('bot_bd.db')
+        c = conn.cursor()
+        c.execute('SELECT NUMBER FROM episodes ORDER BY NUMBER DESC LIMIT 1;')
+        episode_number = c.fetchone()[0]
+        params = {
+            'chat_id': chat_id,
+            'title': 'Эпизод {}: {}'.format(episode_number + 1, title)
+        }
+        method = 'setChatTitle'
+        c.execute('INSERT INTO episodes VALUES (\'{}\', \'{}\');'
+                  .format(episode_number + 1, title))
+        conn.commit()
+        conn.close()
+        response = requests.post(self.__api_url + method, params)
+        return response
+
+    def edit_chat_title(self, chat_id, title):
+        conn = sqlite3.connect('bot_bd.db')
+        c = conn.cursor()
+        c.execute('SELECT NUMBER FROM episodes ORDER BY NUMBER DESC LIMIT 1;')
+        episode_number = c.fetchone()[0]
+        c.execute('UPDATE episodes SET NAME="{}" WHERE NUMBER = '
+                  '(SELECT MAX(NUMBER) FROM episodes)'.format(title))
+        params = {
+            'chat_id': chat_id,
+            'title': 'Эпизод {}: {}'.format(episode_number, title)
+        }
+        method = 'setChatTitle'
+        conn.commit()
+        conn.close()
+        response = requests.post(self.__api_url + method, params)
+        return response       
+
+    def get_chat_title(self, episode_number):
+        try:
+            if(int(episode_number) <= 61):
+                return "Я знаю историю только с 62 эпизода, сорян :("
+            conn = sqlite3.connect('bot_bd.db')
+            c = conn.cursor()
+            c.execute('SELECT NUMBER FROM episodes '
+                      'ORDER BY NUMBER DESC LIMIT 1;')
+            last_episode = c.fetchone()[0]
+            if last_episode < int(episode_number):
+                last_chat_title = 'Такого эпизода еще не было'
+            else:
+                c.execute('SELECT NAME FROM episodes WHERE NUMBER == {};'
+                          .format(episode_number))
+                last_chat_title = 'Эпизод {}: {}'\
+                    .format(episode_number, c.fetchone()[0])
+            c.close()
+        except ValueError:
+            last_chat_title = 'Хорошая попытка, но надо ввести номер ' \
+                'эпизода. Попытайся еще раз - я верю, у тебя все получится!'
+            pass
+        return last_chat_title
+
+    def get_weather(self):
+        webpage_response = requests.get(self.__weather_url)
+        webpage = webpage_response.content
+        soup = BeautifulSoup(webpage, 'html.parser')
+        # get weather description
+        mydivs = soup.findAll("div",
+                              class_="small-12 medium-6 large-7 columns text-center")
+        regex = re.compile(r'>(.*)<')
+        weather = str(regex.findall(str(mydivs[0])))[2:-2]
+        # get temperature
+        mydivs = soup.findAll("div", class_="temperature")
+        regex = re.compile(r'e">(.*)<')
+        temperature = str(regex.findall(repr(mydivs)))[2:-2]
+        return '''Температура за бортом: {}\n{}'''.format(temperature, weather)
+
+    def get_covid(self):
+        response = requests.get(self.__covid_url)
+        info = response.json()['countrydata']
+        day_new = info[0]['total_new_cases_today']
+        day_death = info[0]['total_new_deaths_today']
+        total_cases = info[0]['total_cases']
+        total_deaths = info[0]['total_deaths']
+        return (day_new, total_cases, day_death, total_deaths)
+
+    def greetings(self, group_id):
+        weather = self.get_weather()
+        covid_info = self.get_covid()
+        greetings = 'Доброе утро, господа!\n\n' \
+            '{}\n\n' \
+            'COVID-19 (Россия):\n' \
+            'new: {}\n' \
+            'total: {}\n\n' \
+            'Желаю всем удачного дня!\n\n{}' \
+            .format(weather, covid_info[0], 
+                   covid_info[1], str(self.__now)[:-7])
+        self.send_message(group_id, greetings)
---
+++ Main.py
@@ -0,0 +1,118 @@
+import config
+import ConfBot as Bot
+import requests
+import sqlite3
+import datetime
+
+vanga_pic_url = 'https://s.gorodche.ru/localStorage/news/73' \
+    '/f8/40/8c/73f8408c_resizedScaled_1020to574.jpg'
+
+group_id = config.group_id
+supergroup_id = config.supergroup_id
+my_id = config.my_id
+
+greetings = ('привет', 'здравствуй', 'здравствуйте',
+             'hello', 'здарова', 'yo', 'jo', 'жэвра', 'жевра',
+             'салют', 'здорова', 'йо', 'жо', 'рыцк', 'рыч')
+
+bot_help = '1. /episode <Название эпизода>\n' \
+    'Установить новый эпизод, пишите только название\n\n' \
+    '2. /episode_edit <Название эпизода>\n' \
+    'Редактировать название текущего эпизода\n\n' \
+    '3. /episode_num <Номер эпизода>\n' \
+    'Показать название N эпизода\n\n' \
+    '4. /weather\n' \
+    'Показать погоду в Москве\n\n' \
+    '5. /covid\n' \
+    'Показать статистику по COVID-19 в России'
+
+bot = Bot.Bot(config.token)
+
+
+def common_compare(last_chat_id, last_chat_text):
+    if last_chat_text.lower() == '/weather':
+        weather = bot.get_weather()
+        bot.send_message(last_chat_id, weather)
+    if last_chat_text.lower() == '/covid':
+        covid_info = bot.get_covid()
+        covid_text = 'Коронавирус в России:\n\n' \
+                     'Новые случаи: +{}\n' \
+                     'Всего случаев: {}\n\n' \
+                     'Новые погибшие: +{}\n' \
+                     'Всего погибших: {}' \
+                     .format(covid_info[0], covid_info[1], 
+                     covid_info[2], covid_info[3])
+        bot.send_message(last_chat_id, covid_text)    
+
+
+def group_compare(last_chat_id, last_chat_text, last_update):
+    if last_chat_text.lower() == '/help':
+        bot.send_message(last_chat_id, bot_help)
+    if last_chat_text.lower().startswith('/episode_num '):
+        bot.send_message(last_chat_id,
+                         bot.get_chat_title(last_chat_text.lower()[13:]))
+    elif last_chat_text.lower().startswith('/episode_edit '):
+        bot.edit_chat_title(last_chat_id, last_chat_text[14:])
+    elif last_chat_text.lower().startswith('/episode '):
+        bot.set_chat_title(last_chat_id, last_chat_text[9:])
+
+
+def private_compare(last_chat_id, last_chat_text,
+                    last_chat_name, last_update):
+    if last_chat_text.lower() in greetings:
+        bot.send_message(last_chat_id, 'Привет, твоё имя - {}!'
+                         .format(last_chat_name))
+        bot.send_photo(last_chat_id, vanga_pic_url)
+    if last_chat_text.lower().startswith('/mail'):
+        bot.send_mailing(last_update, group_id,
+                         last_chat_text.lower()[6:])
+
+
+def main():
+
+    new_offset = None
+    timeout = 60
+    now = datetime.datetime.now()
+    today = now.day
+    hour = now.hour
+
+    while True:
+
+        # Send every day greetings
+        if today == now.day and hour == 0:
+            bot.greetings(group_id)
+            today = (datetime.date.today() + datetime.timedelta(days=1)).day
+
+        now = datetime.datetime.now()
+        last_update = bot.last_update(new_offset, timeout)
+
+        if last_update != 'error':
+            last_update_id = last_update['update_id']
+            try:
+                # Get last update parameters
+                last_chat_id = bot.get_chat_id(last_update)
+                last_chat_text = bot.get_chat_text(last_update)
+                last_type = bot.get_type(last_update)
+                last_chat_name = bot.get_chat_name(last_update, last_type)
+                # Private or group chat
+                common_compare(last_chat_id, last_chat_text)
+                # Private chat
+                if last_type == 'private':
+                    private_compare(last_chat_id, last_chat_text,
+                                    last_chat_name, last_update)
+                # Group or supergroup chat
+                elif last_type == 'group' or last_type == 'supergroup':
+                    group_compare(last_chat_id, last_chat_text, last_update)
+            except KeyError:
+                pass
+        try:
+            new_offset = last_update_id + 1
+        except UnboundLocalError:
+            pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
