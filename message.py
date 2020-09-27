f26e85a5b2d4849daf452b26c45117d14cf9c79b
gpolozok <grigory1505@gmail.com>
2020-09-27 15:53:53 +0300

    corrected message fields
    Signed-off-by: gpolozok <grigory1505@gmail.com>

--- message.py
+++ message.py
@@ -1,14 +1,18 @@
 class Message:
 
     def __init__(self, update):
-        self.update_id = update['update_id']
-        self.message_id = update['message']['message_id']
-        self.chat_text = update['message']['text']
-        self.chat_id = update['message']['chat']['id']
-        self.chat_type = update['message']['chat']['type']
-        self.name = update['message']['from']['first_name']
-        self.username = update['message']['from']['username']
-        if self.chat_text.startswith('/'):
+        self.update_id = update.get('update_id')
+        if update.get('message') is not None:
+            message = update.get('message')
+        else:
+            message = update.get('edited_message')
+        self.message_id = message.get('message_id')
+        self.chat_text = message.get('text')
+        self.chat_id = message['chat']['id']
+        self.chat_type = message['chat']['type']
+        self.name = message['from']['first_name']
+        self.username = message['from']['username']
+        if self.chat_text is not None and self.chat_text.startswith('/'):
             split = self.chat_text.split(" ", maxsplit=1)
             self.command, self.text = split \
                 if len(split) > 1 \
