89be98d56f57dff3a260c0bb4e07fff6fd4b787e
gpolozok <grigory1505@gmail.com>
2020-10-04 00:19:36 +0300

    corrected sql queries
    Signed-off-by: gpolozok <grigory1505@gmail.com>

--- confbot.py
+++ confbot.py
@@ -5,7 +5,6 @@ import asyncpg
 import weather
 import covid
 import anekdot
-from person import Person
 from bot import Bot
 
 
@@ -35,31 +34,53 @@ class ConfBot:
             'anekdot': '/anekdot{}'.format(self.bot_name),
             'episode': '/ep',
             'episode_edit': '/ep_edit',
-            'episode_num': '/ep_num'
+            'episode_num': '/ep_num',
+            'new_conflict': '/conflict'
         }
+
+    async def db_connection(self):
+        conn = await asyncpg.connect(
+            host=self.db_host,
+            port=self.db_port,
+            user=self.db_user,
+            password=self.db_pass,
+            database=self.db_name
+        )
+        return conn
+
+    async def get_birthdayboy(self, now):
+        now_month = now.strftime('%m')
+        now_day = now.strftime('%d')
+        conn = await self.db_connection()
+        sql = 'SELECT firstname, username FROM confbot.persons ' \
+            'WHERE (SELECT EXTRACT(DAY FROM birthdate)) = ($1) ' \
+            'AND (SELECT EXTRACT(MONTH FROM birthdate)) = ($2)'
+        data = (int(now_day), int(now_month))
+        b_boy = await conn.fetchrow(sql, *data)
+        await conn.close()
+        if not b_boy:
+            return None
+        return (b_boy[0], b_boy[1])
 
     async def greetings(self):
         now = datetime.datetime.now()
         today = now.day
-        person_list = await self.initialize()
         while True:
             await asyncio.sleep(1200)
             now = datetime.datetime.now()
-            greetings = None
-            for person in person_list:
-                if now.day == (person.birthdate).day and \
-                    now.month == (person.birthdate).month:
-                    greetings = 'Доброе утро, господа!\n\n' \
-                        'Cегодня особенный день - наш добрый друг {} ' \
-                        'отмечает свой День Рождения!\n\n' \
-                        '{}, бот поздравляет тебя и желает ' \
-                        'счастья, любви и процветания!\n\n{}' \
-                        .format(
-                            person.name,
-                            person.username,
-                            await weather.get_weather()
-                        )
-            if greetings is None:
+            b_boy = await self.get_birthdayboy(now)
+            if b_boy:
+                greetings = 'Доброе утро, господа!\n\n' \
+                    'Cегодня особенный день - наш добрый друг {} ' \
+                    'отмечает свой День Рождения!\n\n' \
+                    '{}, бот поздравляет тебя и желает ' \
+                    'счастья, любви и процветания!\n\n{}' \
+                    .format(
+                        b_boy[0],
+                        b_boy[1],
+                        await weather.get_weather()
+                    )
+            else:
                 greetings = 'Доброе утро, господа!\n\n' \
                     '{}\n' \
                     'Желаю всем удачного дня!' \
@@ -68,32 +89,6 @@ class ConfBot:
                 await self.bot.send_message(self.group_id, greetings)
                 today = (datetime.date.today() + datetime.timedelta(days=1)) \
                     .day
-
-    async def initialize(self):
-        person_list = []
-        conn = await asyncpg.connect(
-            host=self.db_host,
-            port=self.db_port,
-            user=self.db_user,
-            password=self.db_pass,
-            database=self.db_name
-        )
-        rows = (await conn.fetch('SELECT id FROM confbot.persons'))
-        for i in rows:
-            person_info = await conn.fetchrow(
-                'SELECT * FROM confbot.persons WHERE id = {}'.format(i['id'])
-            )
-            person = Person(
-                person_info['user_id'],
-                person_info['firstname'],
-                person_info['username'],
-                person_info['birthdate'],
-                person_info['companion'],
-                person_info['compbirthdate']
-            )
-            person_list.append(person)
-        await conn.close()
-        return person_list
 
     async def send_mailing(self, message):
         if message.username == 'grisha1505':
@@ -134,25 +129,18 @@ class ConfBot:
         if title is None:
             await self.bot.send_message(chat_id, 'Надо ввести новое название')
         else:
-            conn = await asyncpg.connect(
-                host=self.db_host,
-                port=self.db_port,
-                user=self.db_user,
-                password=self.db_pass,
-                database=self.db_name
-            )
+            conn = await self.db_connection()
             episode_number = (await conn.fetchrow(
                 'SELECT episode_num FROM confbot.episodes WHERE id = '
                 '(SELECT MAX(id) FROM confbot.episodes)'
             ))[0]
-            new_title = 'Эпизод {}: {}'.format(episode_number + 1, title)
-            await conn.execute(
-                'INSERT INTO confbot.episodes (episode_num, title) '
-                'VALUES ({}, \'{}\');'
-                .format(episode_number + 1, title)
-            )
+            sql = 'INSERT INTO confbot.episodes (episode_num, title) ' \
+                'VALUES (($1), ($2));'
+            data = (episode_number + 1, title)
+            await conn.execute(sql, *data)
             await conn.close()
-            await self.bot.set_chat_title(chat_id, new_title)
+            message = 'Эпизод {}: {}'.format(episode_number + 1, title)
+            await self.bot.set_chat_title(chat_id, message)
 
     async def edit_chat_title(self, **kwargs):
         chat_id = kwargs['chat_id']
@@ -160,13 +148,7 @@ class ConfBot:
         if title is None:
             await self.bot.send_message(chat_id, 'Надо ввести новое название')
         else:
-            conn = await asyncpg.connect(
-                host=self.db_host,
-                port=self.db_port,
-                user=self.db_user,
-                password=self.db_pass,
-                database=self.db_name
-            )
+            conn = await self.db_connection()
             episode_number = (await conn.fetchrow(
                 'SELECT episode_num FROM confbot.episodes '
                 'WHERE id = (SELECT MAX(id) FROM confbot.episodes)'
@@ -176,9 +158,13 @@ class ConfBot:
                 'SET title=\'{}\' WHERE episode_num={}'
                 .format(title, episode_number)
             )
-            new_title = 'Эпизод {}: {}'.format(episode_number, title)
+            sql = 'UPDATE confbot.episodes ' \
+                'SET title=($1) WHERE episode_num=($2)'
+            data = (title, episode_number)
+            await conn.execute(sql, *data)
             await conn.close()
-            await self.bot.set_chat_title(chat_id, new_title)
+            message = 'Эпизод {}: {}'.format(episode_number, title)
+            await self.bot.set_chat_title(chat_id, message)
 
     async def get_chat_title(self, **kwargs):
         chat_id = kwargs['chat_id']
@@ -189,13 +175,7 @@ class ConfBot:
             elif int(episode_number) < 47:
                 chat_title = 'Я знаю историю только с 47 эпизода, сорян :('
             else:
-                conn = await asyncpg.connect(
-                    host=self.db_host,
-                    port=self.db_port,
-                    user=self.db_user,
-                    password=self.db_pass,
-                    database=self.db_name
-                )
+                conn = await self.db_connection()
                 episode = (await conn.fetchrow(
                     'SELECT episode_num FROM confbot.episodes WHERE id = '
                     '(SELECT MAX(id) FROM confbot.episodes)'
@@ -203,11 +183,10 @@ class ConfBot:
                 if int(episode_number) > episode:
                     chat_title = 'Такого эпизода еще не было'
                 else:
-                    title = (await conn.fetchrow(
-                        'SELECT title FROM confbot.episodes '
-                        'WHERE episode_num = {};'
-                        .format(int(episode_number))
-                    ))[0]
+                    SQL = 'SELECT title FROM confbot.episodes ' \
+                        'WHERE episode_num = ($1);'
+                    data = int(episode_number)
+                    title = (await conn.fetchrow(SQL, data))[0]
                     chat_title = 'Эпизод {}: {}'\
                         .format(episode_number, title)
                 await conn.close()
@@ -216,6 +195,41 @@ class ConfBot:
                 'эпизода. Попытайся еще раз - я верю, у тебя все получится!'
             pass
         await self.bot.send_message(chat_id, chat_title)
+
+    async def new_conflict(self, **kwargs):
+        chat_id = kwargs['chat_id']
+        plaintiff_id = kwargs['user_id']
+        text = kwargs['text']
+        if text is not None:
+            split = (kwargs['text']).split(" ", maxsplit=1)
+            defendant_username, reason = split \
+                if len(split) > 1 \
+                else (split[0], 'Отсутствует')
+            conn = await self.db_connection()
+            sql = 'SELECT user_id FROM confbot.persons WHERE username = ($1)'
+            data = defendant_username
+            defendant_id = (await conn.fetchrow(sql, data))[0]
+            if defendant_id is None:
+                message = 'Такого username в конфе нет'
+            elif defendant_id == plaintiff_id:
+                message = 'Нельзя конфликтовать с самим собой'
+            else:
+                sql = 'INSERT INTO confbot.conflicts ' \
+                    '(user1_id, user2_id, reason, solved) ' \
+                    'VALUES (($1), ($2), ($3), FALSE);'
+                data = (plaintiff_id, defendant_id, reason)
+                await conn.execute(sql, *data)
+                sql = 'SELECT username FROM confbot.persons ' \
+                    'WHERE user_id = ($1)'
+                data = plaintiff_id
+                plaintiff_username = (await conn.fetchrow(sql, data))[0]
+                message = 'Внимание! ' \
+                    'Между {} и {} назрел конфликт!\n\n' \
+                    'Причина: {}' \
+                    .format(plaintiff_username, defendant_username, reason)
+        else:
+            message = 'Введите username того, кому хотите объявит конфликт'
+        await self.bot.send_message(chat_id, message)
 
     async def command_handler(self, message):
         triggers = self.triggers
@@ -226,10 +240,15 @@ class ConfBot:
             triggers.get('anekdot'): self.send_anekdot,
             triggers.get('episode'): self.new_chat_title,
             triggers.get('episode_edit'): self.edit_chat_title,
-            triggers.get('episode_num'): self.get_chat_title
+            triggers.get('episode_num'): self.get_chat_title,
+            triggers.get('new_conflict'): self.new_conflict
         }
         if (trigger := command_trigger.get(message.command)) is not None:
-            await trigger(chat_id=message.chat_id, text=message.text)
+            await trigger(
+                chat_id=message.chat_id,
+                user_id=message.user_id,
+                text=message.text
+            )
 
     async def compare(self, message):
         if message.chat_type == 'private' and message.command == '/mail':
